06d3391edb15ccd7de74b8eea20cac7804c36123
jnu3d1 <jnu3d1@rambler.ru>
2022-07-08 17:04:40 +0600

    Реализована возможность просмотра одной задачи Изменения, которые включены в коммит: 	изменено:      webapp/templates/index.html 	новый файл:    webapp/templates/issue.html 	изменено:      webapp/urls.py 	изменено:      webapp/views.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,5 @@ from webapp.views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('issue/<int:pk>/', IssueView.as_view(), name='issue'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -12,3 +12,13 @@ class IndexView(TemplateView):
     def get(self, request):
         issues = Issue.objects.order_by('-status')
         return render(request, 'index.html', {'issues': issues})
+
+
+class IssueView(TemplateView):
+    template_name = 'issue.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        issue = get_object_or_404(Issue, pk=pk)
+        kwargs['issue'] = issue
+        return super().get_context_data(**kwargs)
