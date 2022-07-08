4c8fad2d7057d8fb3d3614e1506f9f3ff3bdc4f0
jnu3d1 <jnu3d1@rambler.ru>
2022-07-08 16:29:31 +0600

    Созданы модели Созданы и применены миграции Модели зарегистрированы в админ-панели Создана фикстура Изменения, которые включены в коммит: 	новый файл:    fixtures/dump.json 	изменено:      webapp/admin.py 	новый файл:    webapp/migrations/0001_initial.py 	изменено:      webapp/models.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from webapp.models import Issue, Status, Type
+
+
+class IssuesAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'status', 'type']
+    list_display_links = ['summary']
+    list_filter = ['status', 'type']
+    search_fields = ['summary']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Issue, IssuesAdmin)
+
+
+class StatusesAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Status, StatusesAdmin)
+
+
+class TypesAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Type, TypesAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+
+    class Meta:
+        abstract = True
+
+
+class Issue(BaseModel):
+    summary = models.CharField(max_length=50, verbose_name='Краткое описание')
+    description = models.TextField(blank=True, max_length=3000, verbose_name='Полное описание')
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, related_name='statuses',
+                               verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, related_name='types', verbose_name='Тип')
+
+    def __str__(self):
+        return f'{self.id} {self.summary} {self.status} {self.type}'
+
+    class Meta:
+        db_table = 'issues'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=12, verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=12, verbose_name='Тип задачи')
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+    class Meta:
+        db_table = 'types'
+        verbose_name = 'Тип задачи'
+        verbose_name_plural = 'Типы задач'
