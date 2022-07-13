adddae86bd724af1ad4586d21f5cf91d38ee0738
jnu3d1 <jnu3d1@rambler.ru>
2022-07-13 16:59:39 +0600

    Изменения, которые включены в коммит: 	изменено:      webapp/forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -8,4 +8,5 @@ class IssueForm(forms.Form):
     summary = forms.CharField(max_length=50, label='Краткое описание')
     description = forms.CharField(max_length=3000, required=False, label='Полное описание', widget=widgets.Textarea())
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Статус')
-    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Типы задачи')
+    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Типы задачи',
+                                           widget=forms.CheckboxSelectMultiple)
