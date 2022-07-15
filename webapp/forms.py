428cb25de408117d6fb72f26f5d9627d3b139d7c
jnu3d1 <jnu3d1@rambler.ru>
2022-07-15 18:16:22 +0600

    Изменения, которые включены в коммит: 	изменено:      webapp/forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,12 +1,32 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
-from webapp.models import Status, Type
+from webapp.models import Issue
 
 
-class IssueForm(forms.Form):
-    summary = forms.CharField(max_length=50, label='Краткое описание')
-    description = forms.CharField(max_length=3000, required=False, label='Полное описание', widget=widgets.Textarea())
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Статус')
-    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label='Типы задачи',
-                                           widget=forms.CheckboxSelectMultiple)
+class IssueForm(forms.ModelForm):
+    class Meta:
+        model = Issue
+        fields = ['summary', 'description', 'status', 'types']
+        widgets = {
+            'description': widgets.Textarea,
+            'types': widgets.CheckboxSelectMultiple
+        }
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if len(summary) > 50:
+            raise ValidationError('Краткое описание не должно превышать 50 символов!')
+        return summary
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) > 50:
+            raise ValidationError('Полное описание не должно превышать 3000 символов!')
+        return description
+
+    def clean(self):
+        if self.cleaned_data.get('summary') == self.cleaned_data.get('description'):
+            raise ValidationError('Полное описание не должно копировать краткое')
+        return super().clean()
