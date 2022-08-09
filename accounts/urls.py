2062fa55edf565f1e9d4c0e0c353aa40e6143548
jnu3d1 <jnu3d1@rambler.ru>
2022-08-09 15:42:23 +0600

    Изменения, которые включены в коммит: 	новый файл:    accounts/forms.py 	новый файл:    accounts/templates/user_create.html 	изменено:      accounts/urls.py 	изменено:      accounts/views.py 	изменено:      webapp/templates/base.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    # path('create/', register_view, name='create'),
+    path('create/', RegisterView.as_view(), name='create'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
+
 
 def login_view(request):
     context = {}
@@ -21,3 +26,34 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:projects')
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
